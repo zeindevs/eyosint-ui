c8df6e6b048e615f398717db7a34781b28b2a199
zeindevs <zeindevs@gmail.com>
2024-04-22 16:52:11 +0700

    feat: add forget password page

---
+++ src/pages/auth/forget-password.tsx
@@ -0,0 +1,68 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import Layout from '@/components/layout'
+import { Button } from '@/components/ui/button'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+const formSchema = z.object({
+  email: z.string().email(),
+})
+
+export default function ForgetPassword() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+  useEffect(() => {
+    window.document.title = 'Forget Password'
+  }, [])
+
+  return (
+    <Layout>
+      <div className="py-16">
+        <div className="mx-auto flex max-w-[400px] flex-col gap-5 rounded-md border p-5">
+          <div>
+            <h1 className="text-3xl font-medium">Forget password</h1>
+            <p className="mt-2 text-sm text-zinc-700 dark:text-zinc-300">
+              Reset password will be send to your email account.
+            </p>
+          </div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="example@gmail.com" {...field} />
+                    </FormControl>
+                    <FormDescription></FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="w-full">
+                Submit
+              </Button>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </Layout>
+  )
+}
